ed1e475e773a052169b13eac446406f24da03733
Priself <905508629@qq.com>
2023-02-23 17:33:47 +0800

    gru-v2

--- RTL/cordic.v
+++ RTL/cordic.v
@@ -25,28 +25,28 @@ assign Z_in[0] = (mode == 0 ? ((~D_in) + 1) : (D_in << 1));
 
 
 np_rhc #(.DATA_WIDTH(DATA_WIDTH), .M_ATANH(32'd386121), .shift(16)) 
-np_rhc_0 (
+np_rhc_3 (
   .clk(clk), 
   .x_in(X_in[0]), .y_in(Y_in[0]), .z_in(Z_in[0]), 
   .x_out(X_in[1]), .y_out(Y_in[1]), .z_out(Z_in[1])  
 );
 
 np_rhc #(.DATA_WIDTH(DATA_WIDTH), .M_ATANH(32'd204353), .shift(8)) 
-np_rhc_1 (
+np_rhc_2 (
   .clk(clk), 
   .x_in(X_in[1]), .y_in(Y_in[1]), .z_in(Z_in[1]), 
   .x_out(X_in[2]), .y_out(Y_in[2]), .z_out(Z_in[2])  
 );
 
 np_rhc #(.DATA_WIDTH(DATA_WIDTH), .M_ATANH(32'd112542), .shift(4)) 
-np_rhc_2 (
+np_rhc_1 (
   .clk(clk), 
   .x_in(X_in[2]), .y_in(Y_in[2]), .z_in(Z_in[2]), 
   .x_out(X_in[3]), .y_out(Y_in[3]), .z_out(Z_in[3])  
 );
 
 np_rhc #(.DATA_WIDTH(DATA_WIDTH), .M_ATANH(32'd63763), .shift(2)) 
-np_rhc_3 (
+np_rhc_0 (
   .clk(clk), 
   .x_in(X_in[3]), .y_in(Y_in[3]), .z_in(Z_in[3]), 
   .x_out(X_in[4]), .y_out(Y_in[4]), .z_out(Z_in[4])  
